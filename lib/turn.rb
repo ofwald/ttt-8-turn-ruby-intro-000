902b9a221a7cdcee714b78c693f00e805b2198fe
Otto Wald <ofwald@gmail.com>
2019-07-24 20:01:35 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -23,7 +23,7 @@ def valid_move?(index)
     else
       return index
     end
-    
+
   end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
