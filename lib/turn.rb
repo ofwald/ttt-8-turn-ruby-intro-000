7c585da3a18771337aad12488bd74a6b3939c57c
Otto Wald <ofwald@gmail.com>
2019-07-24 20:12:16 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -13,7 +13,7 @@ def input_to_index(index)
 end
 
 def valid_move?(board, index)
-    if !index.between?( -1, 8)
+    if !index.between?( 0, 8)
       return false
     else
       return position_taken?(board, index)
